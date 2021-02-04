51915f289ffa78047f26f8dd1862a4940e590360
Matthew Bell <matthewbell1030@gmail.com>
2021-02-03 23:53:18 -0800

    added top-level function randomName to App.js

--- convo/src/App.js
+++ convo/src/App.js
@@ -1,4 +1,13 @@
+import Messages from "./Messages";
 import './App.css';
+
+function randomName() {
+  const adjectives = [];
+  const nouns = [];
+  const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+  const noun = nouns[Math.floor(Math.random() * nouns.length)];
+  return adjective + noun;
+}
 
 function App() {
   return (
